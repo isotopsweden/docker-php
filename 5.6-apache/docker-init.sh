2415f37605dae27b36a44f875359939491013769
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-05-31 20:41:19 +0000

    docker-init.sh edited online with Bitbucket

--- 5.6-apache/docker-init.sh
+++ 5.6-apache/docker-init.sh
@@ -2,7 +2,7 @@
 
 # Run before script if exists.
 if [ -f "$BEFORE_APACHE_FILE" ]; then
-  exec "$BEFORE_APACHE_FILE"
+  eval "$BEFORE_APACHE_FILE"
 fi
 
 sed -i "s#^DocumentRoot /var/www/html\$#DocumentRoot $SITEPATH#" /etc/apache2/apache2.conf
