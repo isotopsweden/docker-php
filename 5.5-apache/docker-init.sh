28a17fd569b69fbcdb47f8e35e314567e741acf0
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-05-31 07:45:49 +0000

    docker-init.sh edited online with Bitbucket

--- 5.5-apache/docker-init.sh
+++ 5.5-apache/docker-init.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
+
+# Run before script if exists.
+if [ -f ./scripts/before-apache.sh ]; then
+	./scripts/before-apache.sh
+fi
+
 sed -i "s#^DocumentRoot /var/www/html\$#DocumentRoot $SITEPATH#" /etc/apache2/apache2.conf
 echo "Replaced document root with $SITEPATH"
 apache2-foreground
