3167f2373c98a992946e587c4e902c2f3c9142f6
Rasmus Bengtsson <rasmus@isotop.se>
2016-08-10 10:46:49 +0200

    Fix php7

--- 7-apache/docker-init.sh
+++ 7-apache/docker-init.sh
@@ -5,6 +5,6 @@ if [ -f "$BEFORE_APACHE_FILE" ]; then
   sh "$BEFORE_APACHE_FILE"
 fi
 
-sed -i "s#^DocumentRoot /var/www/html\$#DocumentRoot $SITEPATH#" /etc/apache2/apache2.conf
+sed -i "s#DocumentRoot /var/www/html\$#DocumentRoot $SITEPATH#" /etc/apache2/sites-enabled/000-default.conf
 echo "Replaced document root with $SITEPATH"
 apache2-foreground
